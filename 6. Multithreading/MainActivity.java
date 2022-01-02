fa7ef327ed1bab0ffb4a1396b86ad235a972b0b0
Sriram Kannan <68460929+Marirs27@users.noreply.github.com>
2022-01-02 23:12:03 +0530

    Create MainActivity.java

---
+++ 6. Multithreading/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.exercise_multi_threading;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView img;
+    Button bt,bt2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img = (ImageView)findViewById(R.id.imageView);
+        bt = (Button)findViewById(R.id.button);
+        bt2 = (Button)findViewById(R.id.button2);
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        img.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                img.setImageResource(R.drawable.zoro1);
+                            }
+                        });
+                    }
+                }).start();
+            }
+        });
+
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        img.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                img.setImageResource(R.drawable.zoro2);
+                            }
+                        });
+                    }
+                }).start();
+            }
+        });
+    }
+}
